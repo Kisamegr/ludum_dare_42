e27973382ca26c8cbd00ad97880dc9948678063e
Stratos Makrigiannakis <stratos.makrigiannakis@gmail.com>
2018-08-11 20:02:54 +0300

    -Added scriptable objects

---
+++ Assets/Scripts/EnemyObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class EnemyObject : ScriptableObject {
+
+  public float hp;
+  public EnemyType type;
+  public float moveSpeed;
+
+
+}
---
+++ Assets/Scripts/LevelObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Level", menuName = "Level")]
+public class LevelObject : ScriptableObject {
+
+  public int id;
+  public EnemySpawnEvent[] enemySpawnEvents;
+
+  [Serializable]
+  public struct EnemySpawnEvent {
+    public EnemySpawn[] enemySpawns;
+  }
+
+  [Serializable]
+  public struct EnemySpawn {
+    public EnemyType type;
+    public int count;
+  }
+}
