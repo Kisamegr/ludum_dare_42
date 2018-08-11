ee5d85d58d49f04f2aebe8dcaaf011117b4849ca
Stratos Makrigiannakis <stratos.makrigiannakis@gmail.com>
2018-08-11 16:44:41 +0300

    -Now closing opposite walls

--- Assets/Scripts/TopDownGame.cs
+++ Assets/Scripts/TopDownGame.cs
@@ -6,14 +6,21 @@ public class TopDownGame : MonoBehaviour {
 
   public float countdown;
   public float maxCountdown;
+  public float wallSpeed = 0.5f;
 
   private MapBorder[] mapBorders;  // Left, Top, Right, Bottom
 
   private Camera mainCamera;
+  private Transform player;
+
+  public enum WallType {
+    Left, Top, Right, Bottom
+  }
 
   // Use this for initialization
   void Start() {
     mainCamera = Camera.main;
+    player = GameObject.FindGameObjectWithTag("Player").transform;
 
     countdown = 100;
     maxCountdown = 100;
@@ -22,20 +29,35 @@ public class TopDownGame : MonoBehaviour {
     Transform wallParent = GameObject.Find("Walls").transform;
 
     mapBorders = new MapBorder[4];
-    mapBorders[0] = new MapBorder(wallParent.Find("Left"), MapBorder.WallType.Left, screenWorldSize.x/2);
-    mapBorders[1] = new MapBorder(wallParent.Find("Top"), MapBorder.WallType.Top, screenWorldSize.y/2);
-    mapBorders[2] = new MapBorder(wallParent.Find("Right"), MapBorder.WallType.Right, screenWorldSize.x/2);
-    mapBorders[3] = new MapBorder(wallParent.Find("Bottom"), MapBorder.WallType.Bottom, screenWorldSize.y/2);
+    mapBorders[0] = new MapBorder(wallParent.Find("Left"), WallType.Left, screenWorldSize.x/2);
+    mapBorders[1] = new MapBorder(wallParent.Find("Top"), WallType.Top, screenWorldSize.y/2);
+    mapBorders[2] = new MapBorder(wallParent.Find("Right"), WallType.Right, screenWorldSize.x/2);
+    mapBorders[3] = new MapBorder(wallParent.Find("Bottom"), WallType.Bottom, screenWorldSize.y/2);
   }
 
   // Update is called once per frame
-  void Update() {
-    for (int i = 0; i<mapBorders.Length; i++) {
-      mapBorders[i].BorderPosition -= Time.deltaTime/5;
-    }
+  void Update() {    
+    MapBorder closestBorder = ClosestBorder();
+    int oppositeIndex = ((int) closestBorder.Type + 2) % 4;
+    mapBorders[oppositeIndex].BorderPosition -= Time.deltaTime * wallSpeed;
 
     CalculateWalls();
   }
+
+  private MapBorder ClosestBorder() {
+    MapBorder closest = mapBorders[0];
+    float minDistance = int.MaxValue;
+    foreach(MapBorder border in mapBorders) {
+      float distance = Vector3.Distance(border.Transform.position, player.position);
+      if (distance < minDistance) {
+        minDistance = distance;
+        closest = border;
+      }
+    } 
+
+    return closest;
+  }
+
 
   void CalculateWalls() {
     Vector3 screenWorldSize = ScreenWorldSize();
@@ -47,28 +69,28 @@ public class TopDownGame : MonoBehaviour {
       Vector3 localScale = mapBorders[i].Transform.localScale;
 
       switch (mapBorders[i].Type) {
-        case MapBorder.WallType.Left: {
+        case WallType.Left: {
             float targetWidth = screenWorldSize.x/2 - mapBorders[i].BorderPosition;
             targetPosition = new Vector3(-mapBorders[i].BorderPosition - targetWidth/2, 0, 0);
             targetScaleX = targetWidth / mapBorders[i].Renderer.size.x;
             targetScaleY = screenWorldSize.y / mapBorders[i].Renderer.size.y;
             break;
           };
-        case MapBorder.WallType.Top: {
+        case WallType.Top: {
             float targetHeight = screenWorldSize.y/2 - mapBorders[i].BorderPosition;
             targetPosition = new Vector3(0, mapBorders[i].BorderPosition + targetHeight/2, 0);
             targetScaleX = screenWorldSize.x / mapBorders[i].Renderer.size.x;
             targetScaleY = targetHeight / mapBorders[i].Renderer.size.y;
             break;
           }
-        case MapBorder.WallType.Right: {
+        case WallType.Right: {
             float targetWidth = screenWorldSize.x/2 - mapBorders[i].BorderPosition;
             targetPosition = new Vector3(mapBorders[i].BorderPosition + targetWidth/2, 0, 0);
             targetScaleX = targetWidth / mapBorders[i].Renderer.size.x;
             targetScaleY = screenWorldSize.y / mapBorders[i].Renderer.size.y;
             break;
           }
-        case MapBorder.WallType.Bottom: {
+        case WallType.Bottom: {
             float targetHeight = screenWorldSize.y/2 - mapBorders[i].BorderPosition;
             targetPosition = new Vector3(0, -mapBorders[i].BorderPosition - targetHeight/2, 0);
             targetScaleX = screenWorldSize.x / mapBorders[i].Renderer.size.x;
@@ -90,10 +112,6 @@ public class TopDownGame : MonoBehaviour {
   }
 
   struct MapBorder {
-    public enum WallType {
-      Left, Top, Right, Bottom
-    }
-
     public float BorderPosition { get; set; }
     public Transform Transform { get; private set; }
     public SpriteRenderer Renderer { get; private set; }
